f5308985c952f9a2289424d05227448c70485038
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-11-21 00:08:21 -0300

    chore: reset the entire app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { App } from './app';
+
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
-);
+const container = document.getElementById('root');
+
+createRoot(container!).render(<App />);
