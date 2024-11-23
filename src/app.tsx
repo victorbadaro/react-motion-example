476f3c6cd4f3ac75c82e1a78a0c96dbc0ef62687
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-11-23 00:10:53 -0300

    chore: change home page styles

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,14 @@ import { Scale } from './components/scale';
 
 export function App() {
 	return (
-		<>
-			<h1>Motion animations</h1>
+		<main className="min-h-screen bg-zinc-950 text-zinc-50">
+			<div className="container mx-auto px-6 py-10">
+				<div className="space-y-8">
+					<h1 className="text-4xl font-semibold">Motion animations</h1>
 
-			<Scale />
-		</>
+					<Scale />
+				</div>
+			</div>
+		</main>
 	);
 }
