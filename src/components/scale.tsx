044411e9313d36792758094ab22923cb0b54bacc
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-11-23 00:19:15 -0300

    feat: add value prop in the scale component to handle the size of the scale

--- src/components/scale.tsx
+++ src/components/scale.tsx
@@ -1,12 +1,19 @@
 import { motion } from 'motion/react';
 
-export function Scale() {
+interface ScaleProps {
+	value?: number;
+}
+
+export function Scale({ value = 1.2 }: ScaleProps) {
 	return (
 		<motion.div
-			className="flex items-center justify-center size-20 rounded-lg font-semibold cursor-pointer bg-lime-400 text-lime-950"
-			whileHover={{ scale: 1.2 }}
+			className="flex flex-col items-center justify-center size-20 rounded-lg font-semibold cursor-pointer bg-lime-400 text-lime-950"
+			whileHover={{
+				scale: value
+			}}
 		>
 			Scale
+			<span className="text-xs font-normal">{value}x</span>
 		</motion.div>
 	);
 }
